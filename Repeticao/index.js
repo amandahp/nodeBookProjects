a239cf7b2da188117e4efd37e9efb4f1d3b432fb
amandahp <amandahoffmann96@gmail.com>
2020-10-11 16:02:21 -0300

    laços

---
+++ Repeticao/index.js
@@ -0,0 +1,12 @@
+//imprime de 0 a 9
+let x = 0;
+console.log(x++);
+console.log(x++);
+console.log(x++);
+console.log(x++);
+console.log(x++);
+console.log(x++);
+console.log(x++);
+console.log(x++);
+console.log(x++);
+console.log(x++);
