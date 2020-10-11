d64d5ae19cd3a8989e9952b496ddce8170aa9697
amandahp <amandahoffmann96@gmail.com>
2020-10-11 13:31:47 -0300

    nodeProjects

---
+++ Aritimetica/index.js
@@ -0,0 +1,18 @@
+//Declaração e inicialização de duas variaveis
+let a = 5;
+let b = 2;
+
+//Exemplos de operações sobre variaveis
+
+console.log('a = ' + a);
+console.log('b = ' + b);
+console.log('a + b = ' + (a+b));
+console.log('a - b =' + (a-b));
+console.log('a*b = ' + (a*b));
+console.log('a/b = ' + (a/b));
+console.log('a % b = ' + (a%b));
+console.log('a++ = ' + (a++));
+console.log('--b = ' + (--b));
+console.log('a = ' + a);
+console.log('b = ' + b);  
+
