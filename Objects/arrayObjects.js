29ef53bad1e749e525c9e4a779329da00dcb048e
amandahp <amandahoffmann96@gmail.com>
2020-10-11 19:32:02 -0300

    laços

---
+++ Objects/arrayObjects.js
@@ -0,0 +1,3 @@
+const clientes = ['cliente1', cliente2,{nome:"cliente3", saldo:5.0, idade:21, gaucho:false}]
+
+console.log(clientes.cliente3.idade);
---
+++ Objects/objectCliente.js
@@ -0,0 +1,9 @@
+//objeto definido em JSON
+const cliente = {
+    nome:'Luiz',
+    saldo:100.0,
+    idade: 29,
+    gaucho: true
+}
+
+console.log(cliente.idade);
