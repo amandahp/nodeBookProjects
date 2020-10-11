131c02b4830dc55ac331977934f2e29228b671ba
amandahp <amandahoffmann96@gmail.com>
2020-10-11 14:46:41 -0300

    stringsMethosds

---
+++ StringsTest/methodsString.js
@@ -0,0 +1,8 @@
+let src = 'Teste';
+console.log(str.length);
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+str = '1';
+console.log(str.parseInt(str));
+str = '1.5';
+console.log(parseFloat(str));
